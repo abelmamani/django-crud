7562ad4fd4fa097b1c619b4d27a53af090be0fc6
Abel Mamani <abelmamani186f@gmail.com>
2023-01-26 13:19:00 -0300

    bootrsatap

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+
 DATABASES = {
-    'default': dj_database_url.config( default='postgresql://postgres:postgres@localhost:5432/mysite',conn_max_age=600    )}
-
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+#DATABASES = {
+    #'default': dj_database_url.config( default='postgresql://#postgres:postgres@localhost:5432/mysite',conn_max_age=600    )}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
